81ed69246acac15f170d2964e6442270a47987bd
antocsan <93600250+antocsan@users.noreply.github.com>
2022-02-24 18:34:02 +0300

    feat:add filter for price and sale

---
+++ src/modules/filter.js
@@ -0,0 +1,35 @@
+import getData from "./getData"
+import renderGoods from "./renderGoods"
+import { priceFilters, hotSaleFilters } from "./filters"
+
+const filter = () => {
+    const minInput = document.getElementById('min')
+    const maxInput = document.getElementById('max')
+    const checkBoxInput = document.getElementById('discount-checkbox')
+    const checkBoxSpan = document.querySelector('.filter-check_checkmark')
+
+
+    minInput.addEventListener('input', () => {
+        getData().then((data) => {
+            renderGoods(priceFilters(data, minInput.value, maxInput.value))
+        })
+    })
+
+    maxInput.addEventListener('input', () => {
+        getData().then((data) => {
+            renderGoods(priceFilters(data, minInput.value, maxInput.value))
+        })
+    })
+    checkBoxInput.addEventListener('change', () => {
+        if (checkBoxInput.checked) {
+            checkBoxSpan.classList.add('checked')
+        } else {
+            checkBoxSpan.classList.remove('checked')
+        }
+        getData().then((data) => {
+            renderGoods(hotSaleFilters(data, checkBoxInput.checked))
+        })
+    })
+
+}
+export default filter
