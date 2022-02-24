3be96b53c7337ccf2ef2b6b9c1710b3206cc6da9
antocsan <93600250+antocsan@users.noreply.github.com>
2022-02-24 20:49:48 +0300

    feat: add cart add items and delete

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,16 +1,70 @@
+import renderCart from "./renderCart"
+
 const cart = () => {
     const cartBtn = document.getElementById('cart')
     const cartModal = document.querySelector('.cart')
     const cartCloseBtn = cartModal.querySelector('.cart-close')
+    const goodsWraper = document.querySelector('.goods')
+    const cartTotal = cartModal.querySelector('.cart-total > span')
+    const cartWraper = document.querySelector('.cart-wrapper')
 
     const openCart = () => {
+        const cart = localStorage.getItem('cart') ?
+            JSON.parse(localStorage.getItem('cart')) : []
+
         cartModal.style.display = 'flex'
+
+        renderCart(cart)
+        cartTotal.textContent = cart.reduce((sum, goodItem) => {
+            return sum + goodItem.price
+        }, 0)
+
+
     }
     const closeCart = () => {
         cartModal.style.display = ''
     }
     cartBtn.addEventListener('click', openCart)
     cartCloseBtn.addEventListener('click', closeCart)
+
+    goodsWraper.addEventListener('click', (event) => {
+        if (event.target.classList.contains('btn-primary')) {
+            const card = event.target.closest('.card')
+            const key = card.dataset.key
+            const goods = JSON.parse(localStorage.getItem('goods'))
+            const cart = localStorage.getItem('cart') ?
+                JSON.parse(localStorage.getItem('cart')) : []
+            const goodItem = goods.find((item) => {
+                return item.id === +key
+            })
+            cart.push(goodItem)
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+        }
+
+    })
+    cartWraper.addEventListener('click', (event) => {
+        if (event.target.classList.contains('btn-primary')) {
+            const cart = localStorage.getItem('cart') ?
+                JSON.parse(localStorage.getItem('cart')) : []
+            const card = event.target.closest('.card')
+            const key = card.dataset.key
+            const index = cart.findIndex((item) => {
+                return item.id === +key
+            })
+            cart.splice(index, 1)
+
+
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+
+            renderCart(cart)
+
+            cartTotal.textContent = cart.reduce((sum, goodItem) => {
+                return sum + goodItem.price
+            }, 0)
+        }
+    })
 }
 
 
